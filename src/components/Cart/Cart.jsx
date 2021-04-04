e538b9243646dd51948e5ef83a0dab50cdf18d7b
chrisbrooksla@gmail.com <chrisbrooksla@gmail.com>
2021-04-03 23:29:17 -0500

    Cart_Buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,7 +3,13 @@ import { Container, Typography, Button, Grid } from "@material-ui/core";
 import useStyles from "./styles";
 import CartItem from "./CartItem/CartItem";
 import {Link} from  'react-router-dom'
-const Cart = ({ cart }) => {
+
+
+const Cart = ({ cart, 
+                handleUpdateCartQty,
+                handleRemoveFromCart,
+                handleEmptyCart,
+}) => {
  
   const classes = useStyles();
 
@@ -19,7 +25,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} />
+            <CartItem item={item} onUpdateCartQty = {handleUpdateCartQty}  onRemoveFromCart = {handleRemoveFromCart}/>
           </Grid>
         ))}
       </Grid>
@@ -34,6 +40,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick = {handleEmptyCart}
           >
             Empty Cart
           </Button>
